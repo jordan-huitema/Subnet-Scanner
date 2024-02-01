27f7447db5dda1472d8eed689ac05f69f718ff44
Jordan <huitema.jordan@gmail.com>
2024-02-01 18:15:24 +1300

    refactored and changed storage method again, looks done now so i can finally get on with the fetching

--- script.js
+++ script.js
@@ -36,25 +36,33 @@ function toObject(input) {
     type == 'object' && Array.isArray(input) ? type = 'array' : null
     // check for sub objects or arrays
     if (type == 'object') {
-        Object.entries(input).forEach(([key,val])=> {
+        Object.entries(input).forEach(([key, val]) => {
             input[key] = toObject(val)
         })
     }
     return input
 }
 
-function onLoad() { // load all stored id elements
-    // Object.keys(storage).forEach(key => {
-    //     console.log(`loaded ${key}`)
-    //     document.getElementById(key).innerHTML = JSON.parse(storage[key])
-    // })
-    // buildTiles()
+function onLoad() { // load all stored elements
+    storage = toObject(storage)
+    // console.log(storage)
+    Object.entries(storage).forEach(([id, idVal]) => {
+        // console.log(id, idVal)
+        Object.entries(idVal).forEach(([cls, clsVal]) => {
+            // console.log(cls, clsVal)
+            Object.entries(clsVal).forEach(([node, nodeVal]) => {
+                document.querySelectorAll(`#${id} .${cls} ${nodeVal.name}`)[node].value = nodeVal.val
+                try { node % 2 == 0 ? addDiv(cls,false) : null } catch {}
+            })
+        })
+    })
+    buildTiles()
 }
 
 function updateStorage() { //Wipe storage and replace with relevant elements
     // Map of DOM, Arrays of classes to select
     storage = {
-        ipRanges: document.querySelectorAll('div.rangeInput > button[onclick="removeDiv(this.parentNode)"]'),
+        ipRanges: document.querySelectorAll('div.rangeInput > input[disabled]'),
         ipWhitelist: document.querySelectorAll(`div.staticIp > button[onclick="removeDiv(this.parentNode)"]`),
         ipBlacklist: document.querySelectorAll(`div.blacklist > button[onclick="removeDiv(this.parentNode)"]`)
     }
@@ -62,21 +70,18 @@ function updateStorage() { //Wipe storage and replace with relevant elements
         if (val.length > 0) {
             let newObj = {}
             Object.entries(val).forEach(([key, val]) => {
-                newObj[key] = {
-                    innerHTML: val.parentNode.innerHTML,
-                    innerText: val.parentNode.innerText
+                newObj[`${val.parentNode.className}`] ? null
+                    : newObj[`${val.parentNode.className}`] = {}
+                newObj[`${val.parentNode.className}`][key] = {
+                    name: val.tagName,
+                    val: val.value
                 }
             })
             storage[key] = newObj
-            // console.log(newObj)
+            localStorage.setItem(key, toString(newObj))
         } else delete storage[key]
     })
-    // localStorage.setItem(key, jsonIsAPain.toString(storage[key]))
-
-    console.log(storage)
-    console.log(toString(storage))
     console.log(toObject(toString(storage)))
-
 }
 
 function toggleSettings() {
@@ -90,7 +95,7 @@ function toggleSettings() {
     }
 }
 
-function addDiv(target) { //Call addDiv("class")
+function addDiv(target, store = true) { //Call addDiv("class")
     // Get target Elm and Clone
     let elm = document.getElementsByClassName(target)
     elm = elm[elm.length - 1]
@@ -112,7 +117,7 @@ function addDiv(target) { //Call addDiv("class")
         }
     })
     elm.insertAdjacentElement('afterend', clone)
-    updateStorage()
+    store ? updateStorage() : null
 }
 
 function removeDiv(target) { // Call removeDiv(this.parentNode) or removeDiv(id)
