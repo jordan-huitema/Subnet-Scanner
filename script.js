63a9d4d3cc125d864556db247fcb13e8fe329ccd
Jordan <huitema.jordan@gmail.com>
2024-02-05 20:08:16 +1300

    worked on fetcher, spent far to long trying to work around cors, ended up ditching site names due to cors issues, refactored code and changed ip ranges to not be stored and instead be an optional scanner that adds responses to static ip list

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let storage = { ...localStorage }
-let portRange = [8000, 9000] // Should replace this with a UI option at some point
+let portRange = [1000, 9999] // Should replace this with a UI option at some point
 
 //JSON wont stringify sub-objects so i have to make this recurssive strigifyer
 function toString(input) {
@@ -56,21 +56,20 @@ function onLoad() { // load all stored elements
                 let nodeSearch = document.querySelectorAll(`#${id} .${cls} ${nodeVal.name}`)
                 nodeSearch[node].value = nodeVal.val
                 node++
-                nodeSearch[node].value = nodeVal.val2
+                cls == 'staticIp' ? addTile(`${nodeVal.val}`) : null
                 addDiv(cls, false)
             })
         })
     })
-    buildTiles()
 }
 
 function updateStorage() { //Wipe storage and replace with relevant elements
     // Map of DOM, Arrays of classes to select
     storage = {
-        ipRanges: document.querySelectorAll('div.rangeInput > input[disabled]'),
         ipWhitelist: document.querySelectorAll(`div.staticIp > input[disabled]`),
         ipBlacklist: document.querySelectorAll(`div.blacklist > input[disabled]`)
     }
+    // console.log(storage)
     // Find all used input fields in listed storage object, Build new entry and update
     Object.entries(storage).forEach(([key, val]) => {
         if (val.length > 0) {
@@ -79,22 +78,17 @@ function updateStorage() { //Wipe storage and replace with relevant elements
                 // Create new key for class if none exists
                 newObj[`${val.parentNode.className}`] ? null : newObj[`${val.parentNode.className}`] = {}
                 // Create new input node entry
-                if ((key % 2) == 1) {
-                    // add every other value to the previous key
-                    newObj[`${val.parentNode.className}`][key - 1].val2 = val.value
-                } else {
                     newObj[`${val.parentNode.className}`][key] = {
                         name: val.tagName,
                         val: val.value,
                         class: val.className
-                    }
                 }
             })
             storage[key] = newObj
             localStorage.setItem(key, toString(newObj))
         } else delete storage[key]
     })
-    // console.log(toObject(toString(storage)))
+    // console.log(storage)
 }
 
 function toggleSettings() {
@@ -119,14 +113,23 @@ function addDiv(target, store = true) { //Call addDiv("class")
         inputs[key].disabled = true
     })
     // Iterate through Elm and Run Conditions
+    // console.log(elm)
     clone.childNodes.forEach((node, idx) => {
         // Clear Inputs
-        node.localName == 'input' ? clone.childNodes[idx].value = "" : null
+        if (node.localName == 'input') {
+            clone.childNodes[idx].value = ""
+            if (idx == 3 && elm.children[2].value.length > 0) {
+                elm.children[0].value = `${elm.children[0].value}:${elm.children[2].value}`
+            }
+        }
+        // Add input fields together
         // Swap last Add Buttons for X Buttons
         if (node.localName == 'button' && node.innerText == 'Add') {
-            let lastElm = elm.getElementsByTagName("button")[0]
-            lastElm.innerText = "X"
-            lastElm.setAttribute('onClick', "removeDiv(this.parentNode)")
+            // Remove text and input nodes, change button node
+            elm.children[1].remove()
+            elm.children[1].remove()
+            elm.children[1].innerText = "X"
+            elm.children[1].setAttribute('onClick', "removeDiv(this.parentNode)")
         }
     })
     elm.insertAdjacentElement('afterend', clone)
@@ -139,115 +142,104 @@ function removeDiv(target) { // Call removeDiv(this.parentNode) or removeDiv(id)
     updateStorage()
 }
 
-function buildTiles() {
-    // console.log(getIp())
-}
-
 function ipList() {
-    let ips = {}
-    // console.log(storage)
-    console.log('Grabbing IP List')
-    Object.entries(storage).forEach(([id, idVal]) => {
-        // console.log(`- ${id}`)
-        Object.entries(idVal).forEach(([cls, clsVal]) => {
-            Object.entries(clsVal).forEach(([ip, ipVal]) => {
-                console.log(`-- ${ipVal.val} to ${ipVal.val2}`)
-                if (typeof ipVal.val2 == 'string') {
-                    // ips.push(ipVal.val)
-                    ips[ipVal.val] = true
-                    // Split Ips for comparison
-                    let a = ipVal.val.split('.'), b = ipVal.val2.split('.'), diff = []
-                    // console.log('a and b =', a, b)
-                    a.forEach((val, idx) => {
-                        // calculate diffrence
-                        diff.push(b[idx] - val)
-                        diff[idx] < 0 ? diff[idx] = diff[idx] + 255 : null
-                    })
-                    // console.log(diff)
-                    // Itterate through diff arr and check diffs
-                    // If there is a diffrence in range
-                    if (diff.reduce((a, c) => a + c) !== 0) {
-
-                        // Itterate through diff and add any ips between differing ranges
-                        const recurssive = (diffIdx = 0, add = false) => { // input [0.0.1.0]
-                            // Check if shift is active
-                            if (add && diffIdx < 4) {
-                                // loop through index of a and itterate until 255
-                                while (a[diffIdx] < 256) {
-                                    // Each itteration, add currect ip and callback to itterate to itterate subnets
-                                    ips[a.join('.')] = true
-                                    recurssive(diffIdx + 1, true)
-                                    a[diffIdx]++
-                                }
-                                a[diffIdx] = 0
-                            } else {
-                                // Check for diffrence, if no move to next idx
-                                if (diff[diffIdx] == 0) {
-                                    recurssive(++diffIdx)
-                                } else {
-                                    // If idx has diffrence, callback with add until diff = 0
-                                    while (diff[diffIdx] > 0) {
-                                        recurssive(diffIdx + 1, true)
-                                        a[diffIdx]++
-                                        ips[a.join('.')] = true
-                                        diff[diffIdx]--
-                                    }
-                                }
-                            }
-                        }
-                        recurssive()
-                    }
-
-                } else ips[ipVal.val] = ipVal.val2
-            })
+    let ips = {}, inputA = document.getElementById("inputA").value, inputB = document.getElementById("inputB").value
+    // console.log(`-- ${inputA} to ${inputB}`)
+    if (typeof inputB == 'string') {
+        ips[inputA] = true
+        // Split Ips for comparison
+        let a = inputA.split('.'), b = inputB.split('.'), diff = []
+        // console.log('a and b =', a, b)
+        a.forEach((val, idx) => {
+            // calculate diffrence
+            diff.push(b[idx] - val)
+            diff[idx] < 0 ? diff[idx] = diff[idx] + 255 : null
         })
-    })
+        // Itterate through diff arr and check diffs
+        // If there is a diffrence in range
+        if (diff.reduce((a, c) => a + c) !== 0) {
+            // Itterate through diff and add any ips between differing ranges
+            const recurssive = (diffIdx = 0, add = false) => { // input [0.0.1.0]
+                // Check if shift is active
+                if (add && diffIdx < 4) {
+                    // loop through index of a and itterate until 255
+                    while (a[diffIdx] < 256) {
+                        // Each itteration, add currect ip and callback to itterate to itterate subnets
+                        ips[a.join('.')] = true
+                        recurssive(diffIdx + 1, true)
+                        a[diffIdx]++
+                    }
+                    a[diffIdx] = 0
+                } else {
+                    // Check for diffrence, if no move to next idx
+                    if (diff[diffIdx] == 0) {
+                        recurssive(++diffIdx)
+                    } else {
+                        // If idx has diffrence, callback with add until diff = 0
+                        while (diff[diffIdx] > 0) {
+                            recurssive(diffIdx + 1, true)
+                            a[diffIdx]++
+                            ips[a.join('.')] = true
+                            diff[diffIdx]--
+                        }
+                    }
+                }
+            }
+            recurssive()
+        }
+
+    } else ips[inputA] = inputB
     return ips
 }
 
-async function fetchPromise(ip) {
-    console.log(ip)
+function addTile(ip) {
+    console.log('addTile', ip)
+    let tile = document.getElementsByClassName('tile')[0]
+    tile.setAttribute('href', ip)
+    tile.children[0].src = `http://${ip}/favicon.ico`
+    tile.children[1].innerText = ip
+    addDiv('tile', false)
+}
+
+function fetchPromise(ip) {
     // Try http first
     return fetch(`http://${ip}/`, { method: 'GET', mode: 'no-cors' })
-        .then(rtn => {// success
-            return { res: true, ip: `http://${ip}/` }
-            // Try https
-        }, err => {
-            return fetch(`https://${ip}/`, { method: 'GET', mode: 'no-cors' })
-                .then(rtn => {// success
-                    return { res: true, ip: `https://${ip}/` }
-                })
+        .then(succ => { // If response received, set new static ip and add tile card
+            document.querySelectorAll(`div.staticIp > input:not([disabled])`)[0].value = ip
+            addTile(ip)
+            addDiv('staticIp')
         })
 }
-// TODO remove awaits so JS can run all querys at once, also add timeout to fetch to avoid hangs on no response
+
 async function fetchIps(listObj) { // INPUT = { "10.1.1.10": 1010, "10.1.1.1": true, "10.1.1.2": true}
     let keys = Object.keys(listObj)
     // Try all urls
-    let arr = []
+    let arr = ['.', '..', '...']
+    let scanLog = document.getElementById('scanText')
     for (i = 0; i < keys.length; i++) {
         const key = keys[i], val = listObj[keys[i]]
         try {
             // try base url
+            scanLog.innerText = `Scanning: ${key}`
+            console.log('trying ', key)
             await fetchPromise(`${key}`)
-                .then(succ => { arr.push(succ); },
-                    fail => { // try url with port
+                .then(succ => { },
+                    async fail => { // try url with port
                         if (typeof val == 'number') {
-                            arr.push(fetchPromise(`${key}:${val}`))
+                            // console.log('port')
+                            await fetchPromise(`${key}:${val}`).catch(err => { })
                         } else { // generate ports within range
+                            // console.log('no port')
                             for (j = portRange[0]; j < portRange[1]; j++) {
-                                arr.push(fetchPromise(`${key}:${j}`))
+                                // console.log('trying ', `${key}:${j}`)
+                                scanLog.innerText = `Scanning: ${key}:${j}`
+                                await new Promise(r => setTimeout(r, 10));
+                                fetchPromise(`${key}:${j}`).catch(err => { })
                             }
                         }
                     })
         } catch (err) { console.log(err) }
     }
-    console.log(arr)
-    await Promise.allSettled(arr).then(resArr => {
-        console.log(resArr)
-        resArr.forEach((val, idx) => {
-            console.log(val.value)
-        })
-    })
+    scanLog.innerText = `Scanning: Done`
 }
 onLoad()
-fetchIps(ipList())
